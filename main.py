4b4ff746f90c438baefca2ba45264208aefac7c1
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-10-01 17:56:25 +0900

    [add] update

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from janome.tokenizer import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import spmatrix
 
 
-# 分割したい日本語の文章のコーパス
-corpus = [
-    "濃厚豚骨ラーメンの魅力を徹底解剖",
-    "秘密のラーメン店巡り、穴場を発見",
-    "季節限定ラーメン特集、おすすめ3選",
-]
-
-
-def split_words(text: str) -> list[str]:
+def text_optimize(text: str) -> str:
     """文章をトークナイズ"""
     # 文章を単語で分割
     tokenizer = Tokenizer()
@@ -23,33 +16,41 @@ def split_words(text: str) -> list[str]:
         surface = token.surface  # type: ignore
         if part_of_speech in ["名詞", "動詞"]:
             tokenized_corpus.append(surface)
-    return tokenized_corpus
+    return " ".join(tokenized_corpus)
 
+
+def execute_important_words(corpus: list[str]):
+    # TF-IDFベクトル化器を初期化
+    tfidf_vectorizer = TfidfVectorizer()
+
+    # TF-IDFベクトルに変換
+    sparse = tfidf_vectorizer.fit_transform(corpus)
+
+    # 各文章から重要な単語を抽出
+    for i, _ in enumerate(corpus):
+        feature_names = tfidf_vectorizer.get_feature_names_out()
+        tfidf_scores = sparse.getrow(i).toarray()[0]
+
+        # TF-IDFスコアが高い上位3つの単語を抽出し、配列の index を生成 -> 抽出
+        # TF = 該当文書における頻出率 × IDF = 全文書における頻出率の低さ
+        important_words_indices = tfidf_scores.argsort()[::-1][:3]
+
+        # index から単語を取得
+        important_words = [feature_names[idx] for idx in important_words_indices]
+
+        print(f"文書 {i + 1} の重要な単語:", important_words)
+
+
+# 分割したい日本語の文章のコーパス
+CORPUS = [
+    "濃厚豚骨ラーメンの魅力を徹底解剖",
+    "秘密のラーメン店巡り、穴場を発見",
+    "季節限定ラーメン特集、おすすめ3選",
+]
 
 # コーパス内の文章をトークナイズしてリストに格納
-tokenized_corpus = [split_words(text) for text in corpus]
-
-# TF-IDFベクトル化器を初期化
-tfidf_vectorizer = TfidfVectorizer()
-
-# トークナイズされたコーパスをTF-IDFベクトルに変換
-tfidf_matrix = tfidf_vectorizer.fit_transform(
-    [" ".join(tokens) for tokens in tokenized_corpus]
-)
-
-# 各文章から重要な単語を抽出
-for i, _ in enumerate(corpus):
-    feature_names = tfidf_vectorizer.get_feature_names_out()
-    tfidf_scores = tfidf_matrix.getrow(i).toarray()[0]
-
-    # TF-IDFスコアが高い上位3つの単語を抽出し、配列の index を生成 -> 抽出
-    # TF = 該当文書における頻出率 × IDF = 全文書における頻出率の低さ
-    important_words_indices = tfidf_scores.argsort()[::-1][:3]
-
-    # index から単語を取得
-    important_words = [feature_names[idx] for idx in important_words_indices]
-
-    print(f"文書 {i + 1} の重要な単語:", important_words)
+tokenized_corpus: list[str] = [text_optimize(text) for text in CORPUS]
+execute_important_words(tokenized_corpus)
 
 # qdrant で近似する文書を引っ張ってきて tf-idf -> 格納。
 # index は都度更新しなければならないと思われる
